af3ea97735484b76ddc792284e7688fb3f6f8160
NayanaArce <nayanathaumaturgo@hotmail.com>
2024-11-12 15:22:44 -0400

    Create calculadora.sh

---
+++ calculadora.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Nome do script: calculadora.sh
+# Descrição: Este script executa cálculos matemáticos básicos.
+# Autor: Nayana Thaumaturgo da Silva Arce <nayanathaumaturgo@hotmail.com>
+# Data de criação: 07/11/2024
+# Última modificação: 08/11/2024
+# Atualização do código: agora com tratamento de erros.
+
+python3 /Users/nayana/modulo1/python/calculadora.py
